5b3a14836111470927cb8cb6aae0f51726c53336
1003674074@student.katho.be <1003674074@student.katho.be@add7116f-de45-cb49-716b-304bdbc92647>
2011-02-17 17:36:40 +0000

    brol

---
+++ trunk/common/prime.c
@@ -0,0 +1,6 @@
+#include "prime.h" 
+
+int extendArray(int size,int increment){
+printf("Extending an extra %i inches, from the current laughable %i inches\n", increment,size);
+
+}
---
+++ trunk/common/prime.h
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <math.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <curses.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
+
+int extendArray(int size,int increment);
+
