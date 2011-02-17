00b382e811fe597a2d8d6bef2234e45dbf84b6fe
1003674074@student.katho.be <1003674074@student.katho.be@add7116f-de45-cb49-716b-304bdbc92647>
2011-02-17 19:37:06 +0000

    more brol

--- trunk/client/main.c
+++ trunk/client/main.c
@@ -1,7 +1,7 @@
 /*made by alex, compile with pthread*/
 #include "../common/prime.h"
 
-int main(int *argc,char **argv){
+int main(int argc,char **argv){
 extendArray(2,5);
 printf("done\n");
 return 0;
--- trunk/common/prime.c
+++ trunk/common/prime.c
@@ -2,5 +2,5 @@
 
 int extendArray(int size,int increment){
 printf("Extending an extra %i inches, from the current laughable %i inches\n", increment,size);
-
+return 0;
 }
